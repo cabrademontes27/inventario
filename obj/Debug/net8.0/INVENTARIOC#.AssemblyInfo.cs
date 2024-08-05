6159e1696c807094c379d180b817ced9e885c5c3
Luis Cabrera <cabrademontes@gmail.com>
2024-08-05 00:50:15 -0600

    .

--- obj/Debug/net8.0/INVENTARIOC#.AssemblyInfo.cs
+++ obj/Debug/net8.0/INVENTARIOC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INVENTARIOC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b92b5ecfb64150a33fa53a5b8230c06e7851771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa4ee8605c946b6873aecfe1761d8d57cfd636f")]
 [assembly: System.Reflection.AssemblyProductAttribute("INVENTARIOC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INVENTARIOC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
